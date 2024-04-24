0ec9a75ff50ac81089e060ac609ccee6255cbf97
Babak <babakcode@gmail.com>
2024-04-24 15:29:52 +0330

    Fixed import issue on api_route.dart

--- lib/route/api_route.dart
+++ lib/route/api_route.dart
@@ -2,6 +2,7 @@ import 'package:vania/vania.dart';
 import 'package:vania_sample/app/http/controllers/home_controller.dart';
 import 'package:vania_sample/app/http/middleware/authenticate.dart';
 import 'package:vania_sample/app/http/middleware/home_middleware.dart';
+import 'package:vania_sample/app/http/middleware/error_response_middleware.dart';
 
 class ApiRoute implements Route {
   @override
