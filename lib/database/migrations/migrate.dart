c00f6c3487db2b786b021a077e3c04fc3e90f782
Javad Zobeidi <j.zobeidi89@gmail.com>
2024-06-13 14:55:10 +0330

    Add dropTables to migrate file

--- lib/database/migrations/migrate.dart
+++ lib/database/migrations/migrate.dart
@@ -3,6 +3,7 @@ import 'package:vania/vania.dart';
 import 'create_user_table.dart';
 
 void main() async {
+  await MigrationConnection().setup();
   await Migrate().registry();
   await MigrationConnection().closeConnection();
   exit(0);
@@ -10,7 +11,10 @@ void main() async {
 
 class Migrate {
   registry() async {
-    await MigrationConnection().setup();
     await CreateUserTable().up();
   }
+
+    dropTables() async {
+		 await CreateUserTable().down();
+	 }
 }
