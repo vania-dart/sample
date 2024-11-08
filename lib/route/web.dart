bbf0741405024df15e56613fefd151d1b998f54e
Javad Zobeidi <j.zobeidi89@gmail.com>
2024-11-08 09:01:26 +0330

    Update sample code

--- lib/route/web.dart
+++ lib/route/web.dart
@@ -4,8 +4,7 @@ class WebRoute implements Route {
   @override
   void register() {
     Router.get("/", () {
-      return Response.html(
-          '<span>Hello World, welcome to Fullstack development with Vania</span>');
+      return Response.html('welcome'.trans({'name':'Vania'}));
     });
   }
 }
