e988fd388023d1af80550fdde9f7fdd0bd4eded5
Javad Zobeidi <j.zobeidi89@gmail.com>
2025-01-08 08:04:22 +0330

    Add Template view

--- lib/route/web.dart
+++ lib/route/web.dart
@@ -4,7 +4,7 @@ class WebRoute implements Route {
   @override
   void register() {
     Router.get("/", () {
-      return Response.html('welcome'.trans({'name':'Vania'}));
+      return view('index');
     });
   }
 }
